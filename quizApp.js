4e3912e97b7fb47f96e750738c96683f96252407
Braxton Christensen <braxton.christensen@gmail.com>
2015-07-20 12:53:05 -0600

    four down

--- quizApp.js
+++ quizApp.js
@@ -6,26 +6,45 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+};
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
+var Question = function(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+};
 
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
+var users = [];
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
+users.push(
+	new User('Braxton', 'braxton.christensen@gmail.com', '1234', 0),
+	new User('Merrick', 'iammerrick@gmail.com', '4321', 0),
+	new User('Sydney', 'smj.christensen@gmail.com', 'asdf', 0)
+	);
 
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
+var questions = [];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -35,14 +54,29 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
+question1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?', ['T', 'F'], 'T', 2);
+question2 = new Question('T/F: JavaScript is just a scripting version of Java', ['T', 'F'], 'F', 2);
+question3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", ['T', 'F'], 'T', 2);
 
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
+questions.push(
+	question1,
+	question2,
+	question3
+	);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
+users.forEach(function(value, i){
+    console.log(users[i]);
+});
+
+questions.forEach(function(value, i){
+    console.log(questions[i]);
+});
 
